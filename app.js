aa69e9b556bc070a2d19811b3d80fc391e558bbf
nikogriffs <nikogriffs@yandex.ru>
2021-08-10 05:23:01 +0700

    создал пользователя

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 /* eslint-disable no-console */
 const express = require('express');
 const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(express.json()); // для собирания JSON-формата
+app.use(express.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+app.use('/users', require('./routes/users'));
+// app.use('/cards', require('./routes/cards'));
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
