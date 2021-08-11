16ec0e45ca5622b4c39e0657417ff89208124244
nikogriffs <nikogriffs@yandex.ru>
2021-08-12 03:37:01 +0700

    Выполнил пункт 4 14ПР

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const {
-  getUsers, getUserId, createUser, updateProfile, updateAvatar,
+  getUsers, getUserId, updateProfile, updateAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUserId);
-router.post('/', createUser);
 router.patch('/me', updateProfile);
 router.patch('/me/avatar', updateAvatar);
 
