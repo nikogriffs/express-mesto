a2dd94ba82421d37a27d17e5e1896352e5503204
nikogriffs <nikogriffs@yandex.ru>
2021-08-14 07:30:33 +0700

    fix2

--- routes/cards.js
+++ routes/cards.js
@@ -5,22 +5,26 @@ const {
 } = require('../controllers/cards');
 
 router.get('/', getCards);
+
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
     link: Joi.string().required(),
   }),
 }), createCard);
+
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().alphanum().length(24),
   }),
 }), deleteCard);
+
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().alphanum().length(24),
   }),
 }), likeCard);
+
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().alphanum().length(24),
